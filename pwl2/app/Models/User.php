eeb83a27b6dcfcc91d97107a611da24431cdce5b
jordy1982 <herpy.422022011@ukrida.ac.id>
2023-10-17 21:44:35 +0700

    my very first commit

--- pwl2/app/Models/User.php
+++ pwl2/app/Models/User.php
@@ -2,16 +2,15 @@
 
 namespace App\Models;
 
-//use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -22,9 +21,6 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
-        'avatar',
-        'occupation',
-        'is_admin'
     ];
 
     /**
--- pwl2/database/migrations/2014_10_12_000000_create_users_table.php
+++ pwl2/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,13 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('avatar')->nullable();
-            $table->string('occupation')->nullable();
-            $table->boolean('is_admin')->default(false);
             $table->rememberToken();
             $table->timestamps();
-            $table->softDeletes();
-            $table->string('city');
         });
     }
 
